84fa17f926794d4c28631790cf20bcff536972b5
Deepak Jangir <deepak@Deepaks-MacBook-Pro.local>
2023-02-12 10:17:54 +0000

    Added CV button

--- my_site/settings.py
+++ my_site/settings.py
@@ -128,7 +128,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
